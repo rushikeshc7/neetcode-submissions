7f85bc2e9243059ea8d2b6b05b4d37c5fc71ccb8
Rushikesh Chopade <46434036+rushikeshc7@users.noreply.github.com>
2026-05-24 14:25:25 +0530

    Add: valid-palindrome-ii - submission-0

---
+++ Data Structures & Algorithms/valid-palindrome-ii/submission-0.js
@@ -0,0 +1,32 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @return {boolean}
+     */
+    validPalindrome(s) {
+
+        function isPalindrome(left, right) {
+            while (left < right) {
+                if (s[left] !== s[right]) {
+                    return false
+                }
+                left++;
+                right--;
+            }
+            return true
+        }
+
+        let left = 0;
+        let right = s.length - 1;
+
+        while (left < right) {
+            if (s[left] !== s[right]) {
+                // Skip either left or right element
+                return isPalindrome(left + 1, right) || isPalindrome(left, right - 1)
+            }
+            left++;
+            right--;
+        }
+        return true;
+    }
+}
