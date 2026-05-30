bfd32ceaf202873f9627bb17427eaa76a71327f2
Rushikesh Chopade <46434036+rushikeshc7@users.noreply.github.com>
2026-05-30 21:17:30 +0530

    Add: sort-an-array - submission-0

---
+++ Data Structures & Algorithms/sort-an-array/submission-0.js
@@ -0,0 +1,39 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number[]}
+     */
+    sortArray(nums) {
+        function mergeSort(arr) {
+            if (arr.length <= 1) return arr;
+            const mid = Math.floor(arr.length / 2);
+            const left = mergeSort(arr.slice(0, mid));
+            const right = mergeSort(arr.slice(mid))
+            return merge(left, right)
+        }
+
+        function merge(left, right) {
+            const result = [];
+            let i = 0;
+            let j = 0;
+            while (i < left.length && j < right.length) {
+                if (left[i] <= right[j]) {
+                    result.push(left[i++])
+                } else {
+                    result.push(right[j++])
+                }
+            }
+
+            while (i < left.length) {
+                result.push(left[i++])
+            }
+
+            while (j < right.length) {
+                result.push(right[j++])
+            }
+            return result
+        }
+
+        return mergeSort(nums)
+    }
+}
